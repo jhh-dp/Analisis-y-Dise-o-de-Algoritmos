c27bb6ed6a75e83ce8fe099646990021425f30c4
Jorman Herrera Hancco <jherrera@ulasalle.edu.pe>
2026-04-20 18:58:08 -0500

    implementacion de BubbleSort

---
+++ Actividad 4/BubbleSort.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class BubbleSort {
+
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        int temp;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // intercambio
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingrese la cantidad de elementos: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        // ingreso de datos
+        for (int i = 0; i < n; i++) {
+            System.out.print("Elemento " + (i + 1) + ": ");
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Arreglo original: " + Arrays.toString(arr));
+
+        bubbleSort(arr);
+
+        System.out.println("Arreglo ordenado: " + Arrays.toString(arr));
+
+        sc.close();
+    }
+}
