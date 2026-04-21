0b0953a141f1a84c39323253383dd95eed383ad3
Jorman Herrera Hancco <jherrera@ulasalle.edu.pe>
2026-04-20 21:37:39 -0500

    mejoras total BubbleSort

--- Actividad 4/BubbleSort.java
+++ Actividad 4/BubbleSort.java
@@ -6,17 +6,27 @@ public class BubbleSort {
     public static void bubbleSort(int[] arr) {
         int n = arr.length;
         int temp;
-        boolean deteccion = false; // FIX IMPORTANTE
+
+        boolean deteccion;
+
+        int comparaciones = 0;
+        int intercambios = 0;
 
         for (int i = 0; i < n - 1; i++) {
             deteccion = false;
 
             for (int j = 0; j < n - i - 1; j++) {
+
+                // contador de comparaciones
+                comparaciones++;
+
                 if (arr[j] > arr[j + 1]) {
+                    // intercambio
                     temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
 
+                    intercambios++; // contador de intercambios
                     deteccion = true;
                 }
             }
@@ -26,12 +36,9 @@ public class BubbleSort {
             }
         }
 
-        // ahora ya es seguro usarla
-        if (deteccion) {
-            System.out.println("Hubo intercambios durante el ordenamiento");
-        } else {
-            System.out.println("El arreglo ya estaba ordenado");
-        }
+        // resultados finales
+        System.out.println("Comparaciones: " + comparaciones);
+        System.out.println("Intercambios: " + intercambios);
     }
 
     public static void main(String[] args) {
